454893364e645c0ee6031370b7310dd07b73d7e9
Caleb Whiting <caleb@whiting.io>
2022-08-18 18:16:03 -0400

    use ifname in systemctl restart command

--- resources/update-tpl.sh
+++ resources/update-tpl.sh
@@ -3,4 +3,4 @@ mv /etc/wireguard/{{ifname}}.conf /etc/wireguard/{{ifname}}-bu.conf
 touch /etc/wireguard/{{ifname}}.conf
 chmod 066 /etc/wireguard/{{ifname}}.conf
 echo "{{config-str}}" | sed "s/__PRIVKEY__/$(sed -e 's/[^a-zA-Z0-9,._+@%-]/\\&/g; 1{$s/^$/""/}; 1!s/^/"/; $!s/$/"/' /etc/wireguard/privkey)/" > /etc/wireguard/{{ifname}}.conf
-systemctl restart wg-quick@wg0
+systemctl restart wg-quick@{{ifname}}
